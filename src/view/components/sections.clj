6e155c5f88359e7d745ab708f929c7816beff3e0
Alexander Olshansky <sander2411ukr@gmail.com>
2025-06-10 13:26:26 +0200

    removed the color filter on the private-clearance and commercial-clearance pages

--- src/view/components/sections.clj
+++ src/view/components/sections.clj
@@ -104,15 +104,7 @@
   [:div.s_ts_outer
    [:div.s_ts
     [:img.s_ts_bg_img {:alt "image" :src img-src}]
-
     [:div.s_ts_bg_img_mask]
-
-    [:div.s_ts_bg_l_filter {:aria-hidden "true"}
-     [:div.s_ts_bg_l_filter_grad]]
-
-    [:div.s_ts_bg_r_filter {:aria-hidden "true"}
-     [:div.s_ts_bg_r_filter_grad {:style "clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%);"}]]
-
     [:div.s_ts_txt_cont
      [:figure
       [:blockquote.s_ts_txt_cont
