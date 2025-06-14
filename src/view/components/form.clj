001e6e26625518bb88d6ac8f6c3a97812c9969c8
Alexander Olshansky <sander2411ukr@gmail.com>
2025-06-14 09:58:32 +0200

    changed the email recipient

--- src/view/components/form.clj
+++ src/view/components/form.clj
@@ -46,7 +46,7 @@
     [:svg {:width 18 :high 18 :fill "#fff" :viewBox "0 0 20 20"}
      [:path {:d "M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"}]]]
 
-   [:div.fw_300.fst_it.ml_10 "Ihre Nachricht wurde zugestellt."]])
+   [:div.c_light.fw_300.fst_it.ml_10 "Ihre Nachricht wurde zugestellt."]])
 
 
 (defn failure-msg []
@@ -55,5 +55,5 @@
     [:svg {:width 18 :high 18 :fill "#fff" :viewBox "0 0 20 20"}
      [:path {:d "M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"}]]]
 
-   [:div.fw_300.fst_it.ml_10 "Etwas ist schiefgelaufen, bitte versuchen Sie es erneut."]])
+   [:div.c_light.fw_300.fst_it.ml_10 "Etwas ist schiefgelaufen, bitte versuchen Sie es erneut."]])
 
